b1d8611ec57672e2f88b5f136f424c8125315335
Prashant Sahni <prashant.sahni5@gmail.com>
2012-03-12 17:47:31 +0530

    -> minor change in setup.js to show medium size images

--- _site/plugins/jflickrfeed/setup.js
+++ _site/plugins/jflickrfeed/setup.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 		qstrings: {
 			id: '76830659@N05'
 		},
-		itemTemplate: '<div class="span4 flicker"><p><a href="{{image_b}}"><img src="{{image_s}}" alt="{{title}}" class="flicker"/></a></p></div>'
+		itemTemplate: '<div class="span4 flicker"><p><a href="{{image_b}}"><img src="{{image_m}}" alt="{{title}}" class="flicker"/></a></p></div>'
 	});
 	
 });
