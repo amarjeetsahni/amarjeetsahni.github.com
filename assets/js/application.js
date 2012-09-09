07bd4f6ef94c45cec71a9b13ba53cafe1d324501
Prashant Sahni <prashant.sahni5@gmail.com>
2012-09-09 17:32:13 +0530

    home redesigned

--- assets/js/application.js
+++ assets/js/application.js
@@ -5,12 +5,25 @@ APPLICATION = {
     var path = window.location.pathname; 
     for(tabs = 0;tabs<APPLICATION.TABS.length;tabs++){
       if( path.match(APPLICATION.TABS[tabs]) ){
-         $("a[uri="+ APPLICATION.TABS[tabs] + "]").addClass('white');
+         $("a[uri='"+ APPLICATION.TABS[tabs] + "']").addClass('white');
          break;
       }  
     }  
   }
 }
+
 $(function(){
   APPLICATION.currentTab();
-})
+  $("#slides").slides({
+		preload: true,
+		play:3000,
+		pause: 3000,
+		slideSpeed: 1000,
+		crossfade:true,
+		generateNextPrev: false,
+		generatePagination: false,
+		preloadImage: 'assets/img/loadimage.gif',
+		generateNextPrev: false,
+		generatePagination: false,
+ }); 
+});
