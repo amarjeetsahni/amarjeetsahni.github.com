86e6cc3a7d944d9829899120fa41d34d83b1acaf
Prashant Sahni <prashant.sahni5@gmail.com>
2012-09-16 19:17:02 +0530

    work page

--- _site/assets/js/application.js
+++ _site/assets/js/application.js
@@ -1,11 +1,11 @@
 APPLICATION = {
-  TABS: ['/', 'about_us', 'contact_us', 'donate', 'our_projects'],
+  TABS: ['/', '/about_us.html', '/contact_us.html', '/donate.html', '/work.html'],
   currentTab: function(){
     var tabs;
     var path = window.location.pathname; 
-    for(tabs = 0;tabs<APPLICATION.TABS.length;tabs++){
-      if( path.match(APPLICATION.TABS[tabs]) ){
-         $("a[uri='"+ APPLICATION.TABS[tabs] + "']").addClass('white');
+    for(tabs = 0;tabs < APPLICATION.TABS.length;tabs++){
+      if( path == APPLICATION.TABS[tabs] ){
+         $("a[href='"+ APPLICATION.TABS[tabs] + "']").addClass('white');
          break;
       }  
     }  
